bbf1d2b38e91929585cd1a04939cb9b094dd4a2d
A. Salgara <asalgara@As-MacBook-Pro.local>
2020-09-27 20:40:26 +0600

    initial

---
+++ WheatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WheatherApp
+//
+//  Created by A. Salgara on 27.09.2020.
+//  Copyright © 2020 A. Salgara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
